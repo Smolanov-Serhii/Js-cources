54637b45e4df8662738fff7d67dd60f91545428e
Sergey <smolanovsergey@gmail.com>
2021-10-01 08:55:39 +0300

    Первый урок

---
+++ scripts.js
@@ -0,0 +1,12 @@
+let title = "Test Title";
+let screens = 50;
+console.log(screens);
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive;
+
+
+alert(title);
+
+console.log("Значение переменной screens = " + screens + "");
