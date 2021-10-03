cdd0991fc5b3e00a9ad1a547ba75a5d3cce24f1a
Sergey <smolanovsergey@gmail.com>
2021-10-03 09:10:41 +0300

    Второй урок

--- scripts.js
+++ scripts.js
@@ -1,12 +1,15 @@
-let title = "Test Title";
-let screens = 50;
-console.log(screens);
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "Занятия по JS";
+console.log("Тип данных title = " + typeof title);
+let fullPrice = 100;
+console.log("Тип данных fullPrice = " + typeof fullPrice);
+let adaptive = true;
+console.log("Тип данных adaptive = " + typeof adaptive);
+let screens = "Простые, Сложные, Интерактивные";
+console.log("Длинне строки screens = " + screens.length);
+let screenPrice = 50;
+console.log("Стоимость вёрстки экранов = " + screenPrice + "грн.");
+let rollback = 20;
+console.log("Стоимость разработки сайиа = " + fullPrice + "грн.");
+console.log("Строка в нижнем регистре = " + screens.toLowerCase());
+console.log("Процент отката = " + fullPrice * (rollback/100) + "%");
 
-
-alert(title);
-
-console.log("Значение переменной screens = " + screens + "");
