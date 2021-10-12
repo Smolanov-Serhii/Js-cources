9d3da8e0df02d3482349103a57ae4862105fb9e6
Sergey <smolanovsergey@gmail.com>
2021-10-12 18:35:21 +0300

    Домашняя работа 6

--- scripts.js
+++ scripts.js
@@ -1,74 +1,30 @@
-window.onload = function () {
-    document.getElementById('jscalc').onclick = function() {
-        let servicePercentPrice = 0;
-        let back = 0;
-        let fullPrice = 0;
+let CurrentDigit = 0;
 
-        function getTitle(title) {
-            let TitleElement = document.querySelector("" + title + "").innerHTML.trim();
-            result = TitleElement.charAt(0).toUpperCase() + TitleElement.slice(1).toLocaleLowerCase();
-            if(result.length > 12) {
-                result = result.slice(0,12) + '...';
-            }
-            document.querySelector("" + title + "").innerText = result;
+function ChackDigit(){
+    let ThinkDigit = 83;
+    function NewDigit(){
+        CurrentDigit = +prompt('Угадай число от 1 до 100');
+        if (CurrentDigit === null) {
+            return;
         }
+        if(!Number.isInteger(Number.parseInt(CurrentDigit))) {
+            alert("Нужно ввести число");
+            NewDigit();
+        };
 
-
-
-        function ChangeDisplay(DomElement, DisplayValue){
-            document.querySelector("" + DomElement + "").style.display = "" + DisplayValue + "";
-        }
-
-        function getAllServicePrices(FirstAddService, SecondAddService, ThirdPrice = 0){
-            let servicePrice1  = Number(document.getElementById('' + FirstAddService +'').value);
-            let servicePrice2  = Number(document.getElementById('' + SecondAddService +'').value);
-            return servicePrice1 + servicePrice2 + ThirdPrice;
-        }
-
-        function QuerySelectorInner(SelectorName, DestinationElem){
-            document.querySelector('' + DestinationElem + '').innerText = document.querySelector('' + SelectorName + '').value;
-        }
-
-        function SetValueToElem(selectorDestinaion, ValueOfElem){
-            document.querySelector('' + selectorDestinaion + '').innerText = ValueOfElem;
-        }
-
-        ChangeDisplay('.modal', 'none');
-        ChangeDisplay('.modal__total', 'block');
-
-        QuerySelectorInner('.project-name', '.modal__total-title');
-        getTitle('.modal__total-title');
-        QuerySelectorInner('input[name="level"]:checked', '.modal__total-level .value');
-        QuerySelectorInner('input[name="cost"]', '.modal__total-sale .value');
-
-
-        back  = Number(document.getElementById('back').value);
-        document.querySelector('.modal__total-back .value').innerText = back;
-        document.querySelector('.modal__total-services .value').innerText = getAllServicePrices('service-cost1' , 'service-cost2');
-        let ProjectAdaptive = document.getElementById("adaptiv").checked;
-        fullPrice = getAllServicePrices('service-cost1' , 'service-cost2', Number(document.getElementById('cost').value));
-
-        document.querySelector('.modal__total-cost .value').innerText = fullPrice;
-        servicePercentPrice = Math.ceil(fullPrice - back);
-        let SalePerc = 0 * 1;
-        let TotalCost = 0;
-        if(fullPrice >= 30000){
-            SalePerc = 10;
-            document.querySelector('.modal__total-sale .value').innerText = SalePerc + "%";
-            TotalCost = servicePercentPrice - (servicePercentPrice * (SalePerc / 100));
-        } else if(fullPrice > 15000 && fullPrice < 30000){
-            SalePerc = 5;
-            document.querySelector('.modal__total-sale .value').innerText = SalePerc + "%";
-            TotalCost = servicePercentPrice - (servicePercentPrice * (SalePerc / 100));
-        } else {
-            SalePerc = "Скидка не предусмотрена";
-            document.querySelector('.modal__total-sale').innerText = SalePerc;
-            TotalCost = servicePercentPrice;
-        }
-
-        SetValueToElem('.modal__total-pay .value', fullPrice);
-        SetValueToElem('.total-calc .value', TotalCost);
+    }
+    NewDigit();
+    if(CurrentDigit > ThinkDigit){
+        console.log('Загаданное число меньше');
+        ChackDigit(CurrentDigit)
+    } else if(CurrentDigit < ThinkDigit){
+        console.log('Загаданное число больше');
+        ChackDigit(CurrentDigit);
+    } else {
+        console.log('Поздравляю, Вы угадали!!!');
+        return;
     }
 }
 
+ChackDigit();
 
