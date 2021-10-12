dc9e36a140cfca64a3e1fd798a0a883a99a0ccc2
Sergey <smolanovsergey@gmail.com>
2021-10-12 14:19:55 +0300

    Домашняя работа 4

--- scripts.js
+++ scripts.js
@@ -1,44 +1,62 @@
 window.onload = function () {
-        document.getElementById('jscalc').onclick = function() {
-            let servicePercentPrice = 0;
-            let back = 0;
-            let fullPrice = 0;
-            console.log(servicePercentPrice);
-            document.querySelector(".modal").style.display = "none";
-            document.querySelector(".modal__total").style.display = "block";
-            let ProjectName = document.getElementById('project-name').value;
-            document.querySelector('.modal__total-title').innerText = ProjectName;
-            let ProjectLevel = document.querySelector('input[name="level"]:checked').value;
-            document.querySelector('.modal__total-level .value').innerText = ProjectLevel;
-            let screenPrice = Number(document.getElementById('cost').value);
-            document.querySelector('.modal__total-sale .value').innerText = screenPrice * 1;
-            back  = Number(document.getElementById('back').value);
-            document.querySelector('.modal__total-back .value').innerText = +back;
-            let servicePrice1  = Number(document.getElementById('service-cost1').value);
-            let servicePrice2  = Number(document.getElementById('service-cost2').value);
-            document.querySelector('.modal__total-services .value').innerText = servicePrice1 + servicePrice2;
-            let ProjectAdaptive = document.getElementById("adaptiv").checked;
-            fullPrice = servicePrice1 + servicePrice2 + screenPrice;
-            document.querySelector('.modal__total-cost .value').innerText = +fullPrice;
-            servicePercentPrice = Math.ceil(fullPrice - back);
-            let SalePerc = 0 * 1;
-            let TotalCost = 0;
-            if(fullPrice >= 30000){
-                SalePerc = 10;
-                document.querySelector('.modal__total-sale .value').innerText = SalePerc + "%";
-                TotalCost = servicePercentPrice - (servicePercentPrice * (SalePerc / 100));
-            } else if(fullPrice > 15000 && fullPrice < 30000){
-                SalePerc = 5;
-                document.querySelector('.modal__total-sale .value').innerText = SalePerc + "%";
-                TotalCost = servicePercentPrice - (servicePercentPrice * (SalePerc / 100));
-            } else {
-                SalePerc = "Скидка не предусмотрена";
-                document.querySelector('.modal__total-sale').innerText = SalePerc;
-                TotalCost = servicePercentPrice;
-            }
-            document.querySelector('.modal__total-pay .value').innerText = fullPrice;
-            document.querySelector('.total-calc .value').innerText = TotalCost;
+    document.getElementById('jscalc').onclick = function() {
+        let servicePercentPrice = 0;
+        let back = 0;
+        let fullPrice = 0;
+
+        function ChangeDisplay(DomElement, DisplayValue){
+            document.querySelector("" + DomElement + "").style.display = "" + DisplayValue + "";
         }
+
+        function getAllServicePrices(FirstAddService, SecondAddService, ThirdPrice = 0){
+            let servicePrice1  = Number(document.getElementById('' + FirstAddService +'').value);
+            let servicePrice2  = Number(document.getElementById('' + SecondAddService +'').value);
+            return servicePrice1 + servicePrice2 + ThirdPrice;
+        }
+
+        function QuerySelectorInner(SelectorName, DestinationElem){
+            document.querySelector('' + DestinationElem + '').innerText = document.querySelector('' + SelectorName + '').value;
+        }
+
+        function SetValueToElem(selectorDestinaion, ValueOfElem){
+            document.querySelector('' + selectorDestinaion + '').innerText = ValueOfElem;
+        }
+
+        ChangeDisplay('.modal', 'none');
+        ChangeDisplay('.modal__total', 'block');
+
+        QuerySelectorInner('.project-name', '.modal__total-title');
+        QuerySelectorInner('input[name="level"]:checked', '.modal__total-level .value');
+        QuerySelectorInner('input[name="cost"]', '.modal__total-sale .value');
+
+
+        back  = Number(document.getElementById('back').value);
+        document.querySelector('.modal__total-back .value').innerText = back;
+        document.querySelector('.modal__total-services .value').innerText = getAllServicePrices('service-cost1' , 'service-cost2');
+        let ProjectAdaptive = document.getElementById("adaptiv").checked;
+        fullPrice = getAllServicePrices('service-cost1' , 'service-cost2', Number(document.getElementById('cost').value));
+
+        document.querySelector('.modal__total-cost .value').innerText = fullPrice;
+        servicePercentPrice = Math.ceil(fullPrice - back);
+        let SalePerc = 0 * 1;
+        let TotalCost = 0;
+        if(fullPrice >= 30000){
+            SalePerc = 10;
+            document.querySelector('.modal__total-sale .value').innerText = SalePerc + "%";
+            TotalCost = servicePercentPrice - (servicePercentPrice * (SalePerc / 100));
+        } else if(fullPrice > 15000 && fullPrice < 30000){
+            SalePerc = 5;
+            document.querySelector('.modal__total-sale .value').innerText = SalePerc + "%";
+            TotalCost = servicePercentPrice - (servicePercentPrice * (SalePerc / 100));
+        } else {
+            SalePerc = "Скидка не предусмотрена";
+            document.querySelector('.modal__total-sale').innerText = SalePerc;
+            TotalCost = servicePercentPrice;
+        }
+
+        SetValueToElem('.modal__total-pay .value', fullPrice);
+        SetValueToElem('.total-calc .value', TotalCost);
+    }
 }
 
 
